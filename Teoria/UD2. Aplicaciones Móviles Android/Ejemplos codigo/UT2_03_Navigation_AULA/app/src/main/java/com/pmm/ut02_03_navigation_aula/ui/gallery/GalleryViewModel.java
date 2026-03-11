98979639db76e1c6fbfd35f01240acae0c63e991
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-03-11 15:35:41 +0100

    Add files via upload

---
+++ Teoria/UD2. Aplicaciones Móviles Android/Ejemplos codigo/UT2_03_Navigation_AULA/app/src/main/java/com/pmm/ut02_03_navigation_aula/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.pmm.ut02_03_navigation_aula.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
